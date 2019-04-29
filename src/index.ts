0459b33040e5cc3ca2a5ef6a3493624113dc0531
Richard Zamora <rzamora@ngvpn01-171-76.dyn.scz.us.nvidia.com>
2019-04-29 13:43:51 -0500

    Show xkcd command on panel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import {
+  Widget
+} from '@phosphor/widgets';
+
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
@@ -8,11 +16,48 @@ import '../style/index.css';
 /**
  * Initialization data for the jupyterlab_nvml extension.
  */
+/*
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyterlab_nvml',
   autoStart: true,
   activate: (app: JupyterLab) => {
     console.log('JupyterLab extension jupyterlab_nvml is activated!');
+  }
+};
+*/
+
+/**
+ * Initialization data for the jupyterlab_xkcd extension.
+ */
+const extension: JupyterLabPlugin<void> = {
+  id: 'jupyterlab_xkcd',
+  autoStart: true,
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
+    console.log('JupyterLab extension jupyterlab_xkcd is activated!');
+
+    // Create a single widget
+    let widget: Widget = new Widget();
+    widget.id = 'xkcd-jupyterlab';
+    widget.title.label = 'xkcd.com';
+    widget.title.closable = true;
+
+    // Add an application command
+    const command: string = 'xkcd:open';
+    app.commands.addCommand(command, {
+      label: 'Random xkcd comic',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.addToMainArea(widget);
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({command, category: 'Tutorial'});
   }
 };
 
